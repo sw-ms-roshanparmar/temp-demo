55fe9c9b10ab7ad37b31395ab624464dc56b6df3
Roshan Parmar <roshan.parmar@softwebsolutions.com>
2019-05-22 13:52:49 +0530

    test

--- application.py
+++ application.py
@@ -158,6 +158,9 @@ for foldername,subfolders,files in os.walk(r"./bookmark/"):
     cnt = 1
     for file in files:
         path = os.path.join(foldername,file)
+		file1 = open('path.txt',"a")
+		file1.write(path)
+		file1.close()
         r = convert_pdf(path, "xml")
         fName = path.split('/')[-1]
         final_fName = fName.split('.')[0]

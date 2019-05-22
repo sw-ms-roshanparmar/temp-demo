e2ea054ba6560d44c4eb3b02667460054cf87a57
Roshan Parmar <roshan.parmar@softwebsolutions.com>
2019-05-22 13:59:09 +0530

    test

--- application.py
+++ application.py
@@ -156,11 +156,13 @@ import PyPDF2
 i1ts = [] 
 for foldername,subfolders,files in os.walk(r"./bookmark/"):
     cnt = 1
+	print('roshan')
     for file in files:
         path = os.path.join(foldername,file)
-		file1 = open('path.txt',"a")
-		file1.write(path)
-		file1.close()
+		print(path)
+		#file1 = open('path.txt',"a")
+		#file1.write(path)
+		#file1.close()
         r = convert_pdf(path, "xml")
         fName = path.split('/')[-1]
         final_fName = fName.split('.')[0]
